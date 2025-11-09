6ab9b5edee91204812bdbced8c7a537ba7375926
ima_ne <dibtimane@gmail.com>
2025-11-09 23:01:37 +0100

    Modify customer insert statement for consistency

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,4 @@
--- task_5.sql
--- Script to insert a single row into the table 'customers'
+USE alx_book_store;
 
-INSERT INTO customers (customer_id, first_name, last_name, email, address)
-VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
