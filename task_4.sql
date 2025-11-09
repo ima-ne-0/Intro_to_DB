46e853f7d222481a0773baa6e7799567dd71b6eb
ima_ne <dibtimane@gmail.com>
2025-11-09 23:00:38 +0100

    Modify SQL script to specify database for table info

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,8 @@
--- task_4.sql
--- Script to print the full description of the table 'books' without using DESCRIBE or EXPLAIN
+USE alx_book_store;
 
 SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Nullable',
-    COLUMN_DEFAULT AS 'Default',
-    COLUMN_KEY AS 'Key',
-    EXTRA AS 'Extra'
+    TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE()  -- Current database passed as argument
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
